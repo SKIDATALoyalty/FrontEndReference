07e0b35eb2f9ed0b3f3bae9fd98999a15eb3f934
SDNET\Diwakar <diwakar.reddy@skidata.com>
2018-04-13 18:17:51 -0700

    Front End Handbook POC Intial Commit
    Front End Handbook POC Intial Commit

---
+++ LoyaltyFrontEndPOC/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('loyalty-front-end-poc App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
