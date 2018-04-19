ea3668a34ba03f828a7c13ed08dcfaed122695a6
SDNET\Diwakar <diwakar.reddy@skidata.com>
2018-04-18 17:52:26 -0700

    Small env configuration chnages
    Small env configuration chnages

--- LoyaltyFrontEndPOC/src/environments/environment.prod.ts
+++ LoyaltyFrontEndPOC/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiKey: 'gtkh01rtXwaQjLX2SZdZtHDoxM7IMm63g00BH3Hj',
-  portalId: 44,
-  redirectUrl: window.location.href,
+  portalId: 31,
+  redirectUrl: window.location.href + '&response_type=token',
   getUrl: 'https://apidev.skidataus.com/44/auth/v1/url',
-  clientId: '7qdnkn70qn5apo3aish8b2aqn3'
+  clientId: 'e43fmc9p2iv1q6k64cejidqnj'
 };
--- LoyaltyFrontEndPOC/src/environments/environment.ts
+++ LoyaltyFrontEndPOC/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   apiKey: 'gtkh01rtXwaQjLX2SZdZtHDoxM7IMm63g00BH3Hj',
-  portalId: 44,
+  portalId: 31,
   redirectUrl: window.location.href + '&response_type=token',
   getUrl: 'https://apidev.skidataus.com/44/auth/v1/url',
   clientId: '7qdnkn70qn5apo3aish8b2aqn3'
