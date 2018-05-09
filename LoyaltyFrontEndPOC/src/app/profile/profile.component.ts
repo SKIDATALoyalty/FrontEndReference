787c1e392c1d7213df8f1bf7708df26fd3b99de2
SDNET\Diwakar <diwakar.reddy@skidata.com>
2018-05-09 13:33:07 -0700

    Profile section changes and added user avatar upload functionality
    Profile section changes and added user avatar upload functionality

--- LoyaltyFrontEndPOC/src/app/profile/profile.component.ts
+++ LoyaltyFrontEndPOC/src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfileService} from './profile.service';
+import {environment} from '../../environments/environment';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {AuthServiceService} from '../auth-service.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,24 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  fileToUpload: File = null;
+  profileInfo: any;
+  avatarUrl: any = 'http://placehold.it/235x235';
 
-  constructor() { }
+  constructor(private profileService: ProfileService,
+              private spinner: NgxSpinnerService,
+              private authService: AuthServiceService) { }
 
   ngOnInit() {
+   this.getProfileInformation();
   }
 
-  readUrl(input) {
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const target: any = e.target;
-        const imgData = target.result;
-        const imgName = input.files[0].name;
-        input.setAttribute('data-title', imgName);
-        console.log(target.result);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    console.log('image this.fileToUpload --', this.fileToUpload);
+  }
+
+  uploadFileToActivity() {
+    this.spinner.show();
+    const imageUploadApiUrl = environment.apidocs + 'v1/API/ProfileImage/Upload/' + this.authService.decodeJwtToken()['custom:UserId'];
+    this.profileService.postFile(this.fileToUpload, imageUploadApiUrl).subscribe(data => {
+        // do something, if upload success
+        console.log('image data--', data);
+        // this.spinner.hide();
+        this.getProfileInformation();
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  getProfileInformation() {
+    this.spinner.show();
+    const profileInfoApiUrl = environment.apidocs + 'v1/API/user';
+    this.profileService.getProfileAPi(profileInfoApiUrl).subscribe(
+      data => {
+        this.spinner.hide();
+        this.profileInfo = data;
+        this.avatarUrl = data['Avatar'] || 'http://placehold.it/235x235';
+        console.log('profileInfo data--', data);
+      },
+      error => {
+        this.spinner.hide();
+        console.log(error);
+      });
   }
 
 }
--- LoyaltyFrontEndPOC/src/app/profile/profile.service.ts
+++ LoyaltyFrontEndPOC/src/app/profile/profile.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import { Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProfileService {
@@ -9,6 +10,12 @@ export class ProfileService {
 
   getProfileAPi(url) {
     return this.http.get(url);
+  }
+
+  postFile(fileToUpload: File, imageUrl: string) {
+    const formData: FormData = new FormData();
+    formData.append('image', fileToUpload, fileToUpload.name);
+    return this.http.post(imageUrl, formData);
   }
 
 }
